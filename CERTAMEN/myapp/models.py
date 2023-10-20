58ef1a064f4874c2c93d688f3bedfbe9022a6e0b
MaikAF <131834284+MaikAF@users.noreply.github.com>
2023-10-20 00:17:55 -0300

    avance del jueves de certamen
    Agregue los models al servidor

--- CERTAMEN/myapp/models.py
+++ CERTAMEN/myapp/models.py
@@ -1,23 +1,33 @@
 from django.db import models
 
+TIPO_CHOICES = [
+    ('S', 'Suspencion de actividades'),
+    ('C', 'Suspencion de clase'),
+    ('I', 'Informacion'),
+]
+
+
 class Entidad(models.Model):
     id = models.BigAutoField(primary_key=True)
     nombre = models.CharField(max_length=50)
-    logo = models.ImageField()
+    logo = models.ImageField(null=True, blank=True)
 
     def __str__(self) -> str:
         return self.nombre
     
-"""
+
 class Comunicado(models.Model):
     id = models.BigAutoField(primary_key=True)
     titulo= models.CharField(max_length=50)
     detalle= models.CharField(max_length=100)
     detalle_corto= models.CharField(max_length=100)
-    entidad=Entidad()
-    visible= bool()
-    fecha_publicacion = date()
-"""
+    tipo = models.CharField(max_length=100, choices=TIPO_CHOICES)
+    entidad=models.ForeignKey(Entidad, on_delete=models.CASCADE)
+    visible = models.BooleanField(True)
+    fecha_publicacion = models.DateTimeField(null=True, blank=True)
+    fecha_ultima_publicacion = models.DateTimeField(null=True, blank=True)
+    #publicado_por =
+
     def __str__(self) -> str:
-        return self.nombre
+        return self.titulo
     
